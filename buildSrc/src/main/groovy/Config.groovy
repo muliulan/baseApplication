fb268a4c8c2ce6c1c49aa18716300f662e39e7a3
zhaochenshuo <953655369@qq.com>
2020-10-14 15:43:05 +0800

    app->moduleCore->base  继承

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -7,27 +7,36 @@ class Config {
     static targetSdkVersion = 29
     static versionCode = 1
     static versionName = "1.0"
+    static kotlin_version = '1.3.72'
 
+    static isBuildModule = false
 
     static depConfig = [
-            //glide 'com.github.bumptech.glide:glide:3.7.0'
-            //图片加载(带有特效)
-            glide                  : "jp.wasabeef:glide-transformations:3.0.1",
-            utilcode               : "com.blankj:utilcode:1.29.0",
-            BaseRecyclerViewAdapter: 'com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.4',
+
             //阿里路由框架
-            arouter                : [
+            arouter     : [
                     "arouter_api"     : 'com.alibaba:arouter-api:1.4.1',
                     "arouter_compiler": "com.alibaba:arouter-compiler:1.2.2"
             ],
-            androidx               : [
-                    appcompat       : 'androidx.appcompat:appcompat:1.2.0',
-                    core_ktx        : 'androidx.core:core-ktx:1.3.2',
-                    constraintlayout: 'androidx.constraintlayout:constraintlayout:2.0.2',
-                    junit           : 'androidx.test.ext:junit:1.1.2',
-                    espresso        : 'androidx.test.espresso:espresso-core:3.3.0'
 
+            library_base: [
+                    //图片加载(带有特效)
+                    glide                  : 'com.github.bumptech.glide:glide:3.7.0',
+                    glide_transformations  : "jp.wasabeef:glide-transformations:4.3.0",
+                    utilcode               : "com.blankj:utilcode:1.29.0",
+                    BaseRecyclerViewAdapter: 'com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.4',
+                    androidx               : [
+                            appcompat       : 'androidx.appcompat:appcompat:1.2.0',
+                            core_ktx        : 'androidx.core:core-ktx:1.3.2',
+                            constraintlayout: 'androidx.constraintlayout:constraintlayout:2.0.2',
+                            junit           : 'androidx.test.ext:junit:1.1.2',
+                            espresso        : 'androidx.test.espresso:espresso-core:3.3.0'
+
+                    ],
+                    junit                  : 'junit:junit:4.12',
+                    kotlin_stdlib_jdk7     : 'org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version'
             ],
+
 
     ]
 
